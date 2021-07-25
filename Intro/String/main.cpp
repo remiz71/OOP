bc86f18d31823ccc25a36b094f3b92c02818c48e
Remi_wr91@itstepru.onmicrosoft.com <Remi_wr91@itstepru.onmicrosoft.com>
2021-07-25 11:13:00 +0300

    classwork

--- Intro/String/main.cpp
+++ Intro/String/main.cpp
@@ -6,8 +6,8 @@ using namespace std;
 class String
 {
 private:
-	int length;
-	char* strg;
+	unsigned int length; //Размер строки в байтах
+	char* str; // Указатель на строку в динамической памяти
 public:
 	int get_length()const
 	{
@@ -15,27 +15,34 @@ public:
 	}
 	string get_string()const
 	{
-		return strg;
+		return str;
 	}
-	String()
+	explicit String(unsigned int length=80)
 	{
-		this->strg = new char[length];
+		this->length = length;
+		this->str = new char[length] {};
 		cout << "DefaultConstruct: " << this << endl;
-		cout << sizeof(strg) << endl;
 	}
-	String(const char strg[])
+	String(const char* str)
 	{
-		this->length = strlen(strg) + 1;
-		this->strg = new char[length] ;
-		for (int i = 0; strg[i]; i++)
-		{
-			this->strg[i] = strg[i];
-		}
-		cout << "Const:" << tab << this << endl;
+		/*while (str[length++]);*/
+		this->length = strlen(str) + 1; //this->length хранит размер в байтах
+										//а strlen() считает в символах
+		this->str = new char[length] {};
+		for (int i = 0; i < length; i++)this->str[i] = str[i];
+		cout << "1ArgConst:" << tab << this << endl;
 	}
 	~String()
 	{
+		delete[] this->str; //[] - удаление массива
 		cout << "Destructor:\t" << this << endl;
+	}
+
+	// Methods
+	void print()const
+	{
+		cout << "Size:" << tab << length << endl;
+		cout << "Str:" << tab << str <<endl;
 	}
 	String& operator=(const String& other)
 	{
@@ -49,11 +56,14 @@ ostream& operator<< (ostream & os, const String & obj)
 }
 int main()
 {
-	String str;
-	String str1{ "Hello" };
-	String str2{ "World" };
-	cout << str1 << endl;
-	cout << str2 << endl;
+	setlocale(LC_ALL, "Russian");
+	//String str = 5; //Implicit Conversion from int to String
+	String str(5);  // explicit constructor можно вызвать только так
+					// и нельзя вызвать =
+	str.print();
+	String str1 = "Hello";
+	str1.print();
+	
 	
 
 }
