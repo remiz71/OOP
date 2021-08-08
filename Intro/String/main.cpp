d4d22df305c42145edc873f70bd31802fa7ae9a8
Remi_wr91@itstepru.onmicrosoft.com <Remi_wr91@itstepru.onmicrosoft.com>
2021-08-08 14:55:03 +0300

    METHODS OUT OF CLASS

--- Intro/String/main.cpp
+++ Intro/String/main.cpp
@@ -16,75 +16,92 @@ private:
 	unsigned int length; //Размер строки в байтах
 	char* str; // Указатель на строку в динамической памяти
 public:
-	unsigned int get_length()const
-	{
-		return length;
-	}
-	const char* get_str()const
-	{
-		return str;
-	}
-	char* get_str()
-	{
-		return str;
-	}
-	explicit String(unsigned int length=80):length(length),str(new char[length] {})
-	{
-		cout << "DefaultConst: " <<tab<<  this << endl;
-	}
-	String(const char* str):String(strlen(str)+1)
-	{
-		for (int i = 0; i < length; i++)this->str[i] = str[i];
-		cout << "1ArgConst:" << tab << this << endl;
-	}
-	String(const String& other):String(other.str)
-	{
-		cout << "CopyConstr:" << tab << this << endl;
-	}
-	~String()
-	{
-		delete[] this->str; //[] - удаление массива
-		cout << "Destructor:\t" << this << endl;
-	}
+	unsigned int get_length()const;
+	const char* get_str()const;
+	char* get_str();
+	explicit String(unsigned int length = 80);
+	String(const char* str);
+	String(const String& other);
+	~String();
 
 	//Operators:
-String& operator+=(const String& other)
+	String& operator+=(const String& other);
+
+	String& operator=(const String& other);
+	const char& operator[](unsigned int i)const;
+	char& operator[](unsigned int i);
+	// Methods:
+
+	void print()const;
+	
+
+};
+unsigned int String::get_length()const
+{
+	return length;
+}
+const char* String::get_str()const
+{
+	return str;
+}
+char* String::get_str()
+{
+	return str;
+}
+String::String(unsigned int length) :length(length), str(new char[length] {})
+{
+	cout << "DefaultConst: " << tab << this << endl;
+}
+String::String(const char* str) :String(strlen(str) + 1)
+{
+	for (int i = 0; i < length; i++)this->str[i] = str[i];
+	cout << "1ArgConst:" << tab << this << endl;
+}
+String::String(const String& other) :String(other.str)
+{
+	cout << "CopyConstr:" << tab << this << endl;
+}
+String::~String()
+{
+	delete[] this->str; //[] - удаление массива
+	cout << "Destructor:\t" << this << endl;
+}
+
+//Operators:
+String& String::operator+=(const String& other)
 {
 	return *this = *this + other;
 }
 
-	String& operator=(const String& other)
-	{
-		// 0. Проверяем , является ли принятый обьект - нашим
-		if (this == &other)return *this;
+String& String::operator=(const String& other)
+{
+	// 0. Проверяем , является ли принятый обьект - нашим
+	if (this == &other)return *this;
 
-		// 1. Освобождаем память, занимаемую старым значением
-		delete[] this->str;
-		// 2. Выполняем deep copy
-		this->length = other.length;
-		this->str = new char[length] {};
-		for (int i = 0; i < length; i++)this->str[i] = other.str[i];
-		cout << "CopyAssigment:" << tab << this << endl;
-		return *this;
-	}
-	const char& operator[](unsigned int i)const
-	{
-		return str[i];
-	}
-	char& operator[](unsigned int i)
-	{
-		return str[i];
-	}
-	// Methods:
+	// 1. Освобождаем память, занимаемую старым значением
+	delete[] this->str;
+	// 2. Выполняем deep copy
+	this->length = other.length;
+	this->str = new char[length] {};
+	for (int i = 0; i < length; i++)this->str[i] = other.str[i];
+	cout << "CopyAssigment:" << tab << this << endl;
+	return *this;
+}
+const char& String::operator[](unsigned int i)const
+{
+	return str[i];
+}
+char& String::operator[](unsigned int i)
+{
+	return str[i];
+}
+// Methods:
 
-	void print()const
-	{
-		cout << "Size:" << tab << length << endl;
-		cout << "Str:" << tab << str <<endl;
-	}
-	
-
-};
+void String::print()const
+{
+	cout << "Size:" << tab << length << endl;
+	cout << "Str:" << tab << str << endl;
+}
 String operator+(const String& left, const String& right)
 {
 	String cat(left.get_length() + right.get_length() - 1);
